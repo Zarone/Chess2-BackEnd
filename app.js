2032b7f03009ee5fde2e3e4687f695f0fc4bb501
Zarone <redninjadolphin1@hotmail.com>
2022-06-25 21:02:05 -0400

    managing sockets

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const cors = require('cors')
-const socket = require('socket.io');
+const socket = require('socket.io')
 
 const port = process.env.PORT || 8080;
 
@@ -10,9 +11,17 @@ var app = express();
 app.use(cors())
 
 const server = http.createServer(app);
-const io = socket(server);
+const io = socket(server, {
+    cors: {
+        origin: process.env.CLIENT,
+        methods: ["GET", "POST"],
+        transports: ['websocket', 'polling'],
+        credentials: true
+    },
+    allowEIO3: true
+});
 
-// contains objects like {p1: pid, p2: pid}
+// contains objects like {p1: {pid, isWhite}, p2: {pid, isWhite}, friendRoom: boolean}
 // index by room ID
 let rooms = {};
 
@@ -22,48 +31,70 @@ let waitingRooms = []
 let roomCount = 0;
 
 io.on('connection', function (socket) {
-    let playerId =  Math.floor((Math.random() * 100) + 1)
+    let playerID =  Math.floor((Math.random() * 100) + 1)
+    let thisRoomID = undefined;
     
-    console.log(playerId + ' connected');
+    console.log(playerID + ' connected');
 
-    socket.on('joined', function (roomId) {
-        console.log("joined", roomId)
-        // // games[roomId] = {}
-        // if (games[roomId].players < 2) {
-        //     games[roomId].players++;
-        //     games[roomId].pid[games[roomId].players - 1] = playerId;
-        // }
-        // else{
-        //     socket.emit('full', roomId)
-        //     return;
-        // }
-        
-        // console.log(games[roomId]);
-        // players = games[roomId].players
-        
+    socket.on('joined', function ({roomID, friendRoom}) {
+        console.log("joined", roomID)
 
-        // if (players % 2 == 0) color = 'black';
-        // else color = 'white';
+        thisRoomID = roomID;
 
-        // socket.emit('player', { playerId, players, color, roomId })
-        // players--;
+        if (rooms[roomID] == undefined){
+            rooms[roomID] = {friendRoom}
+        }
 
+        // if this is the first player to join the room
+        if (rooms[roomID].p1 == undefined){
+            rooms[roomID].p1 = { pid: playerID, isWhite: true };
+
+            socket.emit('player', rooms[roomID].p1 )
+        } else if (rooms[roomID].p2 == undefined){
+            
+            if (playerID == rooms[roomID].p1.pid){
+                playerID++;
+            }
+
+            rooms[roomID].p2 = { pid: playerID, isWhite: false };
+
+            socket.emit('player', rooms[roomID].p2 )
+        } else {
+            console.log("attempting to join full room")
+            return
+        }
+
+        console.log(rooms)
         
     });
-
-    socket.on('move', function (msg) {
-        console.log("move", msg);
-        // socket.broadcast.emit('move', msg);
-    });
-
-    socket.on('play', function (msg) {
-        // socket.broadcast.emit('play', msg);
-        console.log("ready " + msg);
-    });
-
+    
     socket.on('disconnect', function () {
-        console.log(playerId + ' disconnected');
+
+        console.log(playerID + ' disconnected');
+        
+        let thisRoom = rooms[thisRoomID];
+        if (thisRoom && thisRoom.p1 && thisRoom.p2) {
+            if (thisRoom.p1.pid == playerID){
+                socket.broadcast.emit('gameOver', thisRoom.p2.pid);
+            } else {
+                socket.broadcast.emit('gameOver', thisRoom.p1.pid);
+            }
+    
+            delete rooms[thisRoomID]
+        }
+        console.log("rooms", rooms)
+
     }); 
+
+    // socket.on('move', function (msg) {
+    //     console.log("move", msg);
+    //     // socket.broadcast.emit('move', msg);
+    // });
+
+    // socket.on('play', function (msg) {
+    //     // socket.broadcast.emit('play', msg);
+    //     console.log("ready " + msg);
+    // });
 
     
 });
@@ -71,13 +102,26 @@ io.on('connection', function (socket) {
 app.get("/getOpenRoom", (req, res)=>{
     console.log("/getOpenRoom")
     console.log("waitingRooms", waitingRooms)
-    if (waitingRooms.length > 0){
-        res.json({roomID: waitingRooms.shift()});
+    
+    let openRoomID = undefined;
+
+    for (let i = 0; i < waitingRooms.length; i++){
+        console.log(rooms[waitingRooms[i]])
+        if ( !rooms[waitingRooms[i]].friendRoom ){
+            openRoomID = waitingRooms.splice(i, 1);
+            break
+        }
+    }
+
+    if (openRoomID){
+        res.json({roomID: openRoomID});
     } else {
         waitingRooms.push(roomCount);
         roomCount++;
-        res.json({roomID: waitingRooms[0]})
+        res.json({roomID: roomCount})
     }
+
+    console.log("waitingRooms", waitingRooms)
     
 })
 
